76d94031ec5cdf5a00ff3094605eb9627ac221b1
Alok Kumar <emailalosha@gmail.com>
2022-05-06 15:55:30 +0530

    2nd commit

--- vx/build-images.groovy
+++ vx/build-images.groovy
@@ -18,7 +18,7 @@ stages
                     script
                         {
                             
-                           
+                           sh 'ls -ltrh'
                            
                         }
                     }
